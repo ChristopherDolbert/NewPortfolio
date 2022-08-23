0eade570e7da01b9868c892a73971ee0c5af5c9f
Eudes <36993464+SailorEudes@users.noreply.github.com>
2022-08-23 18:52:31 +0200

    Update eustach.js

--- components/apps/eustach.js
+++ components/apps/eustach.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function Eustach() {
     return (
-        <iframe frameBorder="0" src="https://www.eustach.fr/album/3041/sub-zero-project/renaissance-of-rave/embed" className="h-full w-full bg-ub-cool-grey"></iframe>
+        //<iframe frameBorder="0" src="https://www.eustach.fr/artist/137/sub-zero-project" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe scrolling="no" frameborder="no" allow="autoplay" src="https://www.eustach.fr/album/3041/sub-zero-project/renaissance-of-rave/embed" className="h-full w-full bg-ub-cool-grey"></iframe>
     )
 }
 
